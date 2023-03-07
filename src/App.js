098f0760d1c1d38bdcb349d0e02e6b74963a4b08
manojnani166 <116333393+manojnani166@users.noreply.github.com>
2023-03-07 11:00:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
 
-function App() {
+firebase.initializeApp({
+  apiKey: "AIzaSyBDvinj4oE7nEr3VLwCJbO5yI80inFf4Fk",
+  authDomain: "sign-in-popup-86073.firebaseapp.com",
+  projectId: "sign-in-popup-86073",
+  storageBucket: "sign-in-popup-86073.appspot.com",
+  messagingSenderId: "112457780176",
+  appId: "1:112457780176:web:418bd2ccc1504be0c1c7d5"
+});
+
+const auth = firebase.auth();
+
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged(person => {
+      if (person) {
+        setUser(person);
+      } else {
+        setUser(null);
+      }
+    })
+  }, []);
+
+  const signInWithGoogle = async () => {
+    try {
+      await auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const signOut = async () => {
+    try {
+      await auth.signOut();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <center>
+        {
+          user ? (
+            <div>
+              <h1>Welcome to Home</h1>
+              <button onClick={signOut}>Sign Out</button>
+            </div>
+          ) : (
+            <button onClick={signInWithGoogle}>Sign In With Google</button>
+          )
+        }
+      </center>
     </div>
   );
 }
